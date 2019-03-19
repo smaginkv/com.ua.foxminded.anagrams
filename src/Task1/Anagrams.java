5af1ebe26c7ea4499e10b658166cc9860ccdeb41
Smagin Kirill <ywstsc@mail.ru>
2019-03-19 23:41:29 +0500

    Rework task according of your instructions

--- src/Task1/Anagrams.java
+++ src/Task1/Anagrams.java
@@ -1,57 +1,45 @@
 package Task1;
 
-import java.util.*;
-
 public class Anagrams {
-	// maybe it will be useful for visual comparison
-	private String inputString;
-	private String outputString;
-
-	public Anagrams(String source) {
-		inputString = source.replaceAll("  ", " ");
-		outputString = "";
+	public String makeAnagram(String sentence) {
+		return reverseString(sentence);
 	}
 
-	public void process() {
-		outputString = "";
-		//reverse every word in phrase
-		for(String word: inputString.split(" ")) {
-			outputString += reverseWord(word);
+	private String reverseString(String sentence) {
+		String outputSentence = "";
+		for (String word : sentence.split("[\\n|\\r|\\t|\\u0020]+")) {
+			outputSentence += reverseWord(word);
 		}
+		return outputSentence.trim();
 	}
 
-	private String reverseWord(String someWord) {
-		//separate word from another
-		String reverseWord = (outputString == "")? "":" ";		
-		for(int i = 0, j = someWord.length();i < someWord.length();i++) {	
-			if (Character.isLetter(someWord.charAt(i))) {
+	private String reverseWord(String word) {
+		int wordLength = word.length();
+		char[] reversedBuffer = new char[wordLength];
+		char[] buffer = word.toCharArray();
 
-				do {
-				} while (!Character.isLetter(someWord.charAt(--j)));
-				// 1. both indexes point to the letter, get letter from desc index
-				reverseWord += someWord.charAt(j);
-
-				// 2. asc index point to non letter symbol, it stay on the same place
-			} else
-				reverseWord += someWord.charAt(i);
+		for (int i = 0; i < wordLength; i++) {
+			reversedBuffer[wordLength - 1 - i] = buffer[i];
 		}
-		return  reverseWord;
-	}
-	
-	@Override
-	public String toString() {
-		return outputString;
-	}
 
-	public String getInputString() {
-		return inputString;
+		for (int i = 0, j = 0; i < wordLength; i++) {
+			if (Character.isLetter(buffer[i])) {
+				while (!Character.isLetter(reversedBuffer[j++])) {
+				}
+				;
+
+				buffer[i] = reversedBuffer[j - 1];
+			}
+		}
+
+		return String.copyValueOf(buffer) + '\u0020';
 	}
 
 	public static void main(String[] args) {
-		Anagrams anagram = new Anagrams("a1bcd2ef1 3ghij 4  5");
-
-		// to run calculations in constructor it is not best idea
-		anagram.process();
-		System.out.println(anagram.toString());
+		Anagrams anagram = new Anagrams();
+		String sentenceBefore = "a1bcd2ef1 3ghij" + '\n' + "4  5";
+		String sentenceAfter = anagram.makeAnagram(sentenceBefore);
+		System.out.println("Sentence before: "+sentenceBefore);
+		System.out.println("Sentence after: "+sentenceAfter);
 	}
 }
