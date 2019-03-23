d2acaf6fa5cdf50218b9af95e9c47b8abb8c70fd
kirill smagin <ywstsc@mail.ru>
2019-03-23 14:33:35 +0500

    Add comments and change name RegExp constant

--- src/ua/com/foxminded/anagrams/Anagram.java
+++ src/ua/com/foxminded/anagrams/Anagram.java
@@ -2,11 +2,11 @@ package ua.com.foxminded.anagrams;
 
 public class Anagram {
 
-    final String SPACESEXPRESSION = "[\\n|\\r|\\t|\\u0020]+";
+    final String ONE_OR_MULTIPLE_WHITESPACES_REGEXP = "[\\n|\\r|\\t| ]+";
 
     public String makeAnagram(String sentence) {
         String outputSentence = "";
-        for (String word : sentence.split(SPACESEXPRESSION)) {
+        for (String word : sentence.split(ONE_OR_MULTIPLE_WHITESPACES_REGEXP)) {
             outputSentence += reverseWord(word);
         }
         return outputSentence.trim();
@@ -14,19 +14,24 @@ public class Anagram {
 
     private String reverseWord(String word) {
         int wordLength = word.length();
+        // two buffers used for change place letter for one travel
         char[] reversedBuffer = new char[wordLength];
         char[] buffer = word.toCharArray();
 
+        // fill reverse buffer
         for (int i = 0; i < wordLength; i++) {
             reversedBuffer[wordLength - 1 - i] = buffer[i];
         }
 
+        // travel direct buffer
         for (int i = 0, j = 0; i < wordLength; i++) {
             if (Character.isLetter(buffer[i])) {
-                while (!Character.isLetter(reversedBuffer[j++])) {
+                while (!Character.isLetter(reversedBuffer[j])) {
+                    j++;
                 }
                 ;
-                buffer[i] = reversedBuffer[j - 1];
+                // if it is letter change to reverse letter
+                buffer[i] = reversedBuffer[j++];
             }
         }
 
