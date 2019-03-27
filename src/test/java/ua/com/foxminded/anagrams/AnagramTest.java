b27634bc1db1dd8ade58d165e0a8f0ff13501706
kirill.smagin <ywstsc@mail.ru>
2019-03-27 21:44:38 +0500

    Add new and reorganize existing tests

--- src/test/java/ua/com/foxminded/anagrams/AnagramTest.java
+++ src/test/java/ua/com/foxminded/anagrams/AnagramTest.java
@@ -7,16 +7,16 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class AnagramTest {
-    Anagram anagram;
+    private Anagram anagram;
 
     @BeforeEach
-    void beforeEachInitialize() {
+    void setup() {
         anagram = new Anagram();
     }
 
     @Nested
-    @DisplayName("Strategy: happy end ")
-    class HappyEnd {
+    @DisplayName("Strategy: happy path ")
+    class HappyPath {
         @Test
         void ShouldMakeAnagramCorrectWhen1Word() {
             assertEquals("edc1ba", anagram.makeAnagram("abc1de"));
@@ -31,31 +31,7 @@ class AnagramTest {
         void ShouldMakeAnagramCorrectWhen3Word() {
             assertEquals("cb1a 2fed3 j4ihj", anagram.makeAnagram("ab1c 2def3 j4hij"));
         }
-    }
 
-    @Nested
-    @DisplayName("Strategy: Error path")
-    class ErrorPath {
-        // no error planed
-    }
-
-    @Nested
-    @DisplayName("Strategy: ñorner cases")
-    class CornerCases {
-        @Test
-        void ShouldMakeAnagramCorrectWhenEmptyWord() {
-            assertEquals("", anagram.makeAnagram(""));
-        }
-
-        @Test
-        void ShouldMakeAnagramCorrectWhenWordIsUnusualSymbols() {
-            assertEquals("!@#1$%^&2*()_+3", anagram.makeAnagram("!@#1$%^&2*()_+3"));
-        }
-    }
-
-    @Nested
-    @DisplayName("Strategy: white spaces")
-    class Whitespaces {
         @Test
         void ShouldMakeAnagramCorrectWhen2WordAndTab() {
             assertEquals("fhdg12f fhfhhg3", anagram.makeAnagram("fgdh12f" + '\t' + "ghhfhf3"));
@@ -87,4 +63,64 @@ class AnagramTest {
         }
     }
 
+    @Nested
+    @DisplayName("Strategy: ñorner cases")
+    class CornerCases {
+        @Test
+        void ShouldMakeAnagramCorrectWhenEmptyWord() {
+            assertEquals("", anagram.makeAnagram(""));
+        }
+
+        @Test
+        void ShouldMakeAnagramCorrectWhenWordIsUnusualSymbols() {
+            assertEquals("!@#1$%^&2*()_+3", anagram.makeAnagram("!@#1$%^&2*()_+3"));
+        }
+    }
+
+    @Nested
+    @DisplayName("Strategy: white spaces")
+    class Whitespaces {
+        @Test
+        void ShouldMakeAnagramCorrectWhen2WordAnd2Whitespaces() {
+            assertEquals("fhdg12f fhfhhg3", anagram.makeAnagram("fgdh12f  ghhfhf3"));
+        }
+
+        @Test
+        void ShouldMakeAnagramCorrectWhen3WordAnd2Whitespaces() {
+            assertEquals("cb1a 2fed3 j4ihj", anagram.makeAnagram("ab1c  2def3  j4hij"));
+        }
+
+        @Test
+        void ShouldMakeAnagramCorrectWhen2WordAnd2Tabs() {
+            assertEquals("fhdg12f fhfhhg3", anagram.makeAnagram("fgdh12f" + '\t' + '\t' + "ghhfhf3"));
+        }
+
+        @Test
+        void ShouldMakeAnagramCorrectWhen3WordAnd2Tabs() {
+            assertEquals("cb1a 2fed3 j4ihj",
+                    anagram.makeAnagram("ab1c" + '\t' + '\t' + "2def3" + '\t' + '\t' + "j4hij"));
+        }
+
+        @Test
+        void ShouldMakeAnagramCorrectWhen2WordAnd2CarriageReturns() {
+            assertEquals("fhdg12f fhfhhg3", anagram.makeAnagram("fgdh12f" + '\r' + '\r' + "ghhfhf3"));
+        }
+
+        @Test
+        void ShouldMakeAnagramCorrectWhen3WordAnd2CarriageReturns() {
+            assertEquals("cb1a 2fed3 j4ihj",
+                    anagram.makeAnagram("ab1c" + '\r' + '\r' + "2def3" + '\r' + '\r' + "j4hij"));
+        }
+
+        @Test
+        void ShouldMakeAnagramCorrectWhen2WordAnd2Newlines() {
+            assertEquals("fhdg12f fhfhhg3", anagram.makeAnagram("fgdh12f" + '\n' + '\n' + "ghhfhf3"));
+        }
+
+        @Test
+        void ShouldMakeAnagramCorrectWhen3WordAnd2Newlines() {
+            assertEquals("cb1a 2fed3 j4ihj",
+                    anagram.makeAnagram("ab1c" + '\n' + '\n' + "2def3" + '\n' + '\n' + "j4hij"));
+        }
+    }
 }
